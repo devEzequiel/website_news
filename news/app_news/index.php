64567426eb2572d0581bdc5c8821a124be9c51ad
Ezequiel Oliveira <esofinney@gmail.com>
2021-03-18 23:34:08 -0300

    Update index.php

--- news/app_news/index.php
+++ news/app_news/index.php
@@ -2,12 +2,10 @@
 
     require_once "database/connect.php";
 
-
     $query = "SELECT * FROM `noticias`";
     $exe = mysqli_query ($conn, $query);
     $value = mysqli_fetch_all($exe, MYSQLI_ASSOC);
     $b= count($value);
-
 
 ?>
 
@@ -18,10 +16,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="css/style.css"> 
-    <link rel="stylesheet" href="css/stylemenu.css">
-    <link rel="stylesheet" href="css/stylenoticias.css">  
-    <link rel="stylesheet" href="css/footer.css"> 
+    <link rel="stylesheet" href="<?php echo BASE_URL?>css/style.css"> 
+    <link rel="stylesheet" href="<?php echo BASE_URL?>css/stylemenu.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL?>css/stylenoticias.css">  
+    <link rel="stylesheet" href="<?php echo BASE_URL?>css/footer.css"> 
     <title>Noticias</title>
 </head>
 <body id="body">
@@ -45,11 +43,11 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="js/search.js"></script>
-    <script type="text/javascript" src="js/displayNews.js"></script>
-    <script type="text/javascript" src="js/acessNew.js"></script>
-    <script type="text/javascript" src="js/deleteNew.js"></script>
-    <script type= "text/javascript" src="js/opacity_menu.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL?>js/search.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL?>js/displayNews.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL?>js/acessNew.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL?>js/deleteNew.js"></script>
+    <script type= "text/javascript" src="<?php echo BASE_URL?>js/opacity_menu.js"></script>
 </body>
 
 </html>
