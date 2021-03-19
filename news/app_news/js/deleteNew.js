a87530598ae14e46c646a4553b41cdfbbb01a263
Ezequiel Oliveira <esofinney@gmail.com>
2021-03-18 23:34:05 -0300

    Update deleteNew.js

--- news/app_news/js/deleteNew.js
+++ news/app_news/js/deleteNew.js
@@ -1,13 +1,13 @@
 function deleteNew(value) {
-	$.ajax({
-		type: 'get',
-		datatype: 'text',
-		url: './database/deleteNew.php',
-		data: {id: value}
-	}).done(function(result){
-		$('#newletter').replaceWith("");
-		alert (result);
-		window.location.href= "./index.php"
-
-	})
+	if(window.confirm("Você deseja deletar esse registro?")){
+		$.ajax({
+			type: 'get',
+			datatype: 'text',
+			url: './database/deleteNew.php',
+			data: {id: value}
+		}).done(function(result){
+			//$('#newletter').replaceWith("");
+			window.location.href= "./index.php"
+		})
+	}
 }
